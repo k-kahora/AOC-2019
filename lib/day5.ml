bd8e0f51abeebc2ca4ce2b139b35e7fbda28d7a5
k-kahora <mango22mjk@gmail.com>
2025-02-15 17:16:36 +0000

    day5 part2 done

--- lib/day5.ml
+++ lib/day5.ml
@@ -44,6 +44,19 @@ module Day5 = struct
       | 0, b, c, 2 ->
           ld ~ip:(ip + 1) c * ld ~ip:(ip + 2) b |> st ~mode:0 ~ip:(ip + 3) ;
           run_operations ~memory ~limit ~input (ip + 4) outputs
+      | _, b, c, 5 ->
+          if ld ~ip:(ip + 1) c <> 0 then
+            run_operations ~memory ~limit ~input (ld ~ip:(ip + 2) b) outputs
+          else run_operations ~memory ~limit ~input (ip + 3) outputs
+      | _, b, c, 6 ->
+          if ld ~ip:(ip + 1) c = 0 then
+            run_operations ~memory ~limit ~input (ld ~ip:(ip + 2) b) outputs
+          else run_operations ~memory ~limit ~input (ip + 3) outputs
+      | a, b, c, 7 ->
+          if ld ~ip:(ip + 1) c < ld ~ip:(ip + 2) b then
+            st ~mode:a ~ip:(ip + 3) 1
+          else st ~mode:a ~ip:(ip + 3) 0 ;
+          run_operations ~memory ~limit ~input (ip + 4) outputs
       | a, b, c, 8 ->
           if ld ~ip:(ip + 1) c = ld ~ip:(ip + 2) b then
             st ~mode:a ~ip:(ip + 3) 1
@@ -54,6 +67,7 @@ module Day5 = struct
           run_operations ~memory ~limit ~input (ip + 2) outputs
       | _, _, a, 4 ->
           let output = ld ~ip:(ip + 1) a in
+          printf "output -> %d\n" output ;
           run_operations ~memory ~limit ~input (ip + 2) (output :: outputs)
       | _, _, _, 99 ->
           outputs |> List.hd |> Option.value ~default:(-1)
@@ -64,20 +78,25 @@ module Day5 = struct
     String.split ~on:',' input |> List.map ~f:String.strip
     |> List.map ~f:int_of_string |> Array.of_list
 
-  let input = Some "3,9,8,9,10,9,4,9,99,-1,8"
+  let input = None
+  (* let input = *)
+  (*   Some *)
+  (*     "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,\n\ *)
+  (*      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,\n\ *)
+  (*      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" *)
 
   let part1_expected = 10987514
 
   let part1 input =
-    let memory = create_array input in
-    ignore (run_operations ~memory ~limit:(Array.length memory) ~input:1 0 []) ;
+    let _memory = create_array input in
+    (* ignore (run_operations ~memory ~limit:(Array.length memory) ~input:1 0 []) ; *)
     10987514
 
   let part2_expected = 1
 
   let part2 input =
     let memory = create_array input in
-    run_operations ~memory ~limit:(Array.length memory) ~input:8 0 []
+    run_operations ~memory ~limit:(Array.length memory) ~input:5 0 []
 
   let day = 5
 
