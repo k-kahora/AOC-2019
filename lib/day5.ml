4536948af205167e82f2a758ad3aa977d262a329
k-kahora <mango22mjk@gmail.com>
2025-02-15 01:06:51 +0000

    pre debugging

--- lib/day5.ml
+++ lib/day5.ml
@@ -10,18 +10,39 @@ module Day5 = struct
 
   type int_list = int list [@@deriving show]
 
+  type string_list = string list [@@deriving show]
+
+  let add_mult ~memory p1 p2 p3 inst =
+    printf "add_mult -> %d" p3 ;
+    ( match inst with
+    | '1' ->
+        p1 + p2
+    | '2' ->
+        p1 * p2
+    | _ ->
+        failwith "non recognized opcode" )
+    |> string_of_int
+    |> fun a -> memory.(p3) <- a
+
   let create_array input =
     String.split ~on:',' input |> List.map ~f:String.strip
 
   (* run memory is what we update input is the starting value position is the current memory output is the list of otuputs and code is the current code *)
-  let mode ~parameter memory mode_bit =
+  let mode ~p memory mode_bit =
+    printf "mode p -> %s" p ;
     match mode_bit with
     | '0' ->
-        memory.(parameter |> int_of_string)
+        memory.(p |> int_of_string)
     | '1' ->
-        parameter
+        p
     | _ ->
         failwith "no a recognized mode bit"
+
+  let execute_opcode ~memory ~f a b c inst param1 param2 param3 =
+    let param3 = mode ~p:param3 memory a |> int_of_string in
+    let param2 = mode ~p:param2 memory b |> int_of_string in
+    let param1 = mode ~p:param1 memory c |> int_of_string in
+    f ~memory param1 param2 param3 inst
 
   let rec run memory input outputs code =
     match code with
@@ -33,18 +54,20 @@ module Day5 = struct
         run memory input outputs tail
     | "4" :: adress :: tail ->
         run memory input ((adress |> int_of_string) :: outputs) tail
-    | opcode :: param1 :: param2 :: param3 :: tail when List.length tail = 3
-      -> (
-      match String.to_list opcode with
-      | [b; c; _; inst] ->
-          let x = mode memory c ~parameter:param1 |> int_of_string in
-          let y = mode memory b ~parameter:param2 |> int_of_string in
-          let result = if Char.( = ) inst '1' then x + y else x * y in
-          memory.(param3 |> int_of_string) <- result |> string_of_int ;
-          run memory input outputs tail
-      | _ ->
-          failwith "empty opcode" )
-    | _ ->
+    | opcode :: param1 :: param2 :: param3 :: tail -> (
+        printf "\ncurrent opcode -> %s\n" opcode ;
+        match String.to_list opcode with
+        | [b; c; _; inst] ->
+            execute_opcode '0' b c inst param1 param2 param3 ~f:add_mult ~memory ;
+            run memory input outputs tail
+        | [inst] ->
+            execute_opcode '0' '1' '1' inst param1 param2 param3 ~f:add_mult
+              ~memory ;
+            run memory input outputs tail
+        | _ ->
+            failwith "empty opcode" )
+    | a ->
+        show_string_list a |> printf "%s" ;
         failwith "invalid opcode"
 
   let input = None
@@ -54,6 +77,7 @@ module Day5 = struct
   let part1 input =
     let codes = create_array input in
     let memory = codes |> Array.of_list in
+    printf "array length -> %d" (Array.length memory) ;
     let _outputs = run memory "1" [] codes in
     10
 
